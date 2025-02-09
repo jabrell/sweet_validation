986c71dbb1e8370da51aa8a729802bf2fa3e1951
jabrell <4337825+jabrell@users.noreply.github.com>
2025-02-09 11:48:58 +0100

    SchemaManager (#24)
    * changed storage from ABC to protcol
    
    * changed storage from ABC to protcol
    
    * ...
    
    * in-memory relation manager using sqlite
    
    * remove memory registry
    
    * remove memory registry
    
    * remove memory registry
    
    * typing
    
    * typing
    
    * file based sqlite relation manager
    
    * renaming

---
+++ sweet_validation/schema_manager/schema_manager.py
@@ -0,0 +1,187 @@
+from __future__ import annotations
+
+from collections.abc import Generator
+from contextlib import contextmanager
+from typing import Any
+
+from sqlalchemy import Engine, create_engine, event
+from sqlalchemy.orm import Session, sessionmaker
+
+from .models import Base, Data, Schema  # Import your models
+
+__all__ = ["SchemaManager"]
+
+
+@event.listens_for(Engine, "connect")  # type: ignore
+def set_sqlite_pragma(dbapi_connection: Any, connection_record: Any) -> None:
+    """Enable foreign key support for SQLite connections
+
+    Only needed for sqlite connections
+
+    see: https://docs.sqlalchemy.org/en/20/dialects/sqlite.html#foreign-key-support"""
+    if (
+        dbapi_connection.__class__.__module__ == "sqlite3"
+    ):  # Check if it's an SQLite connection
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
+
+
+__all__ = ["SchemaManager"]
+
+
+class SchemaManager:
+    """A simple relation manager based on SQLite
+
+    The underlying database is in-memory if no filename is provided.
+    If a filename is provided, the database is stored in the file.
+    The database has two tables: Schema and Data. Schema
+
+    Attributes:
+        engine (Engine): SQLAlchemy engine
+        SessionLocal (sessionmaker): Session factory
+
+    Methods:
+        get_session: Provides a context-managed database session
+        close_engine: Close the database engine
+        insert_schema: Insert a schema into the database
+        insert_data: Insert data into the database
+        list_schemas: Fetch all schema keys
+        list_data: Fetch all data
+        delete_schema: Delete a schema given the key
+        delete_data: Delete data given the key
+        get_data_schema: Get the schema key associated with the data key
+        close: Close the database engine
+        clear_all: Clear all data in the database
+        clear_and_close: Clear all data and close the database engine
+    """
+
+    _conn_str: str
+
+    def __init__(self, fn: str | None = None) -> None:
+        """Initialize the database engine and session factory
+        Args:
+            fn (str | None): Filename of the sqlite database
+                Defaults to None, which uses an in-memory database
+        """
+        if fn:
+            self._conn_str = f"sqlite:///{fn}"
+        else:
+            self._conn_str = "sqlite:///:memory:"
+
+        self.engine = create_engine(self._conn_str)
+        Base.metadata.create_all(self.engine)  # Create tables if they don't exist
+        self.SessionLocal = sessionmaker(bind=self.engine)  # Create session factory
+
+    @contextmanager
+    def get_session(self) -> Generator[Session, None, None]:
+        """Provides a context-managed database session."""
+        session = self.SessionLocal()
+        try:
+            yield session
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            raise e  # Re-raise after rollback
+        finally:
+            session.close()
+
+    def close_engine(self) -> None:
+        """Close the database engine."""
+        self.engine.dispose()
+
+    def insert_schema(self, id: str) -> None:
+        """Insert a schema into the database given the key
+
+        Args:
+            id (str): Schema key
+
+        Raises:
+            IntegrityError: If the primary key is violated
+        """
+        with self.get_session() as session:
+            session.add(Schema(id=id))
+            session.commit()
+
+    def insert_data(self, id: str, id_schema: str) -> None:
+        """Insert data into the database given the key and key of associated schema
+
+        Args:
+            id (str): Data key
+            id_schema (str): Schema key
+
+        Raises:
+            IntegrityError: If the primary key or foreign constraint is violated
+        """
+        with self.get_session() as session:
+            session.add(Data(id=id, id_schema=id_schema))
+            session.commit()
+
+    def list_schemas(self) -> list[str]:
+        """Fetch all schema keys
+
+        Returns:
+            list[str]: List of schema keys
+        """
+        with self.get_session() as session:
+            return [schema.id for schema in session.query(Schema).all()]
+
+    def list_data(self) -> list[tuple[str, str]]:
+        """Fetch all data
+
+        Returns:
+            list[tuple[str, str]]: List of data tuples (id, id_schema)
+        """
+        with self.get_session() as session:
+            return [(data.id, data.id_schema) for data in session.query(Data).all()]
+
+    def delete_schema(self, id: str) -> None:
+        """Delete a schema given the key
+
+        Args:
+            id (str): Schema key
+
+        Raises:
+            IntegrityError: If the foreign key constraint of data is violated,
+                i.e. data associated with the schema still exist
+        """
+        with self.get_session() as session:
+            session.query(Schema).filter(Schema.id == id).delete()
+            session.commit()
+
+    def delete_data(self, id: str) -> None:
+        """Delete data given the key
+
+        Args:
+            id (str): Data key
+        """
+        with self.get_session() as session:
+            session.query(Data).filter(Data.id == id).delete()
+            session.commit()
+
+    def get_data_schema(self, id: str) -> str:
+        """Get the schema key associated with the data key
+
+        Args:
+            id (str): Data key
+
+        Returns:
+            str: Schema key
+        """
+        with self.get_session() as session:
+            data = session.query(Data).filter(Data.id == id).first()
+            return str(data.id_schema)
+
+    def close(self) -> None:
+        self.close_engine()
+
+    def clear_all(self) -> None:
+        """Clear all data in the database"""
+        with self.get_session() as session:
+            session.query(Data).delete()
+            session.query(Schema).delete()
+            session.commit()
+
+    def clear_and_close(self) -> None:
+        self.clear_all()
+        self.close()
---
+++ sweet_validation/tests/test_schema_manager.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+
+import pytest
+from sqlalchemy.exc import IntegrityError
+
+from ..schema_manager import SchemaManager
+
+db_file = Path("tmp.db")
+
+
+@pytest.fixture(autouse=True, scope="module")
+def cleanup_db_file():
+    yield
+    db_path = Path(db_file)  # Use Pathlib to represent the file
+    if db_path.exists():
+        db_path.unlink()  # Delete the file if it exists
+
+
+@pytest.mark.parametrize("fn", [None, db_file])
+def test_insert_schema(fn: str):
+    relation_manager = SchemaManager(fn=fn)
+    assert relation_manager.list_schemas() == []
+    relation_manager.insert_schema(id="test")
+    assert relation_manager.list_schemas() == ["test"]
+    # raise IntegrityError due to unique constraint of primary key
+    with pytest.raises(IntegrityError):
+        relation_manager.insert_schema("test")
+    relation_manager.clear_and_close()
+
+
+@pytest.mark.parametrize("fn", [None, db_file])
+def test_insert_data(fn: str):
+    relation_manager = SchemaManager(fn=fn)
+    relation_manager.insert_schema(id="test")
+    relation_manager.insert_data(id="test", id_schema="test")
+    assert relation_manager.list_data() == [("test", "test")]
+    # raise IntegrityError due to unique constraint of primary key
+    with pytest.raises(IntegrityError):
+        relation_manager.insert_data("test", "test")
+    # raise exception due to foreign key constraint
+    with pytest.raises(IntegrityError):
+        relation_manager.insert_data("test2", "test2")
+    relation_manager.clear_and_close()
+
+
+@pytest.mark.parametrize("fn", [None, db_file])
+def test_delete_schema(fn: str):
+    relation_manager = SchemaManager(fn=fn)
+    relation_manager.insert_schema(id="test")
+    assert relation_manager.list_schemas() == ["test"]
+    relation_manager.delete_schema(id="test")
+    assert relation_manager.list_schemas() == []
+    # raise exception due to foreign key constraint
+    relation_manager.insert_schema(id="test")
+    relation_manager.insert_data(id="test", id_schema="test")
+    with pytest.raises(IntegrityError):
+        relation_manager.delete_schema(id="test")
+    relation_manager.clear_and_close()
+
+
+@pytest.mark.parametrize("fn", [None, db_file])
+def test_delete_data(fn: str):
+    relation_manager = SchemaManager(fn=fn)
+    relation_manager.insert_schema(id="test")
+    relation_manager.insert_data(id="test", id_schema="test")
+    assert relation_manager.list_data() == [("test", "test")]
+    relation_manager.delete_data(id="test")
+    assert relation_manager.list_data() == []
+    relation_manager.clear_and_close()
+
+
+@pytest.mark.parametrize("fn", [None, db_file])
+def test_get_data_schema(fn: str):
+    relation_manager = SchemaManager(fn=fn)
+    relation_manager.insert_schema(id="s_test")
+    relation_manager.insert_data(id="test", id_schema="s_test")
+    assert relation_manager.get_data_schema(id="test") == "s_test"
+    relation_manager.clear_and_close()
+
+
+def init_from_existing_db():
+    relation_manager = SchemaManager(fn=db_file)
+    relation_manager.insert_schema(id="s_test")
+    relation_manager.insert_data(id="test", id_schema="s_test")
+    relation_manager.close()
+    relation_manager2 = SchemaManager(fn=db_file)
+    assert relation_manager2.get_data_schema(id="test") == "s_test"
+    assert relation_manager2.list_schemas() == ["s_test"]
+    assert relation_manager2.list_data() == [("test", "s_test")]
+    relation_manager2.clear_and_close()
